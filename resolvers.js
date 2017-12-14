d55caa3601c239dcbb423fefa2c18e274232b11a
garricknorthover <garricknorthover@gmail.com>
2017-12-15 10:28:54 +1300

    nah aync back as queries are not promises with mongoose

--- resolvers.js
+++ resolvers.js
@@ -1,7 +1,7 @@
 export default {
     Query: {
-        allCats: (parent, args, { Cat }) => {
-            const cats = Cat.find()
+        allCats: async (parent, args, { Cat }) => {
+            const cats = await Cat.find()
             return cats.map((objToStr) => {
                 objToStr._id = objToStr._id.toString()
                 return objToStr
@@ -9,17 +9,17 @@ export default {
         }
     },
     Mutation: {
-        createCat: (parent, args, { Cat }) => {
-            const kitty = new Cat(args).save();
+        createCat: async (parent, args, { Cat }) => {
+            const kitty = await new Cat(args).save();
             kitty._id = kitty._id.toString()
             return kitty
         },
-        updateCat: (parent, args, { Cat }) => {
-            return Cat.findByIdAndUpdate(args._id, args, { new: true })
-
+        updateCat: async (parent, args, { Cat }) => {
+            const result = await Cat.findByIdAndUpdate(args._id, args, {new:true})
+            return result
         },
         deleteCat: async (parent, args, { Cat }) => {
-            Cat.remove(args)
+            await Cat.remove(args)
             return args
         }
     }
